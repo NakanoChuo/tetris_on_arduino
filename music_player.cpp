b01f305c7daeb76d3374b8f5916a6ea42f660027
NakanoChuo <nakano.chyo.yc@tut.jp>
2022-06-04 20:03:20 +0900

    Refactor

--- music_player.cpp
+++ music_player.cpp
@@ -5,6 +5,17 @@
 #define BUZZER_PIN  7
 #define SOUND_RATE  0.9f  /* 1拍子あたりの音を鳴らす割合（複数の拍子も1音に繋がって聞こえる）*/
 
+// 1音データ
+typedef struct {
+  unsigned value    : 3;  // 音価　　　　　0:倍音符、1:全音符、2:2分音符、3:4分音符、...、7:64分音符
+  unsigned padding1 : 1;
+  unsigned dot      : 2;  // 付点　　　　　0:付点なし(1倍)、1:付点(1.5倍)、2:2付点(1.75倍)、3:3付点(1.875倍)
+  unsigned slur     : 1;  // スラー　　　　0:なし、1:あり
+  unsigned padding2 : 1;
+  unsigned pitch    : 4;  // 音階　　　　　0:休符、1:ド、2:ド#、3:レ、...、12:シ、13~:終了コード
+  signed octave     : 4;  // オクターブ　　-8~-1:低オクターブ、0:オクターブ変動なし、1~7:高オクターブ
+} note_data;
+
 static const unsigned int frequency_list[] PROGMEM = {
   262,  // ド
   277,  // ド♯
@@ -20,6 +31,8 @@ static const unsigned int frequency_list[] PROGMEM = {
   494,  // シ
 };
 static const char PITCH_COUNT = sizeof(frequency_list) / sizeof(*frequency_list);
+
+static bool parse_note(word note, unsigned int bpm, float& ret_note_duration, unsigned int& ret_frequency, bool& is_slur, bool& is_rest); // 音符／休符の解析
 
 
 MusicPlayer::MusicPlayer(void) {
@@ -40,6 +53,13 @@ void MusicPlayer::play(bool is_looped) {
 }
 
 
+// 初めから再生
+void MusicPlayer::restart(void) {
+  this->note_index = 0;
+  this->is_playing = true;
+}
+
+
 // 停止
 void MusicPlayer::stop(void) {
   this->pause();
@@ -54,52 +74,67 @@ void MusicPlayer::pause(void) {
 }
 
 
-// 音を出力する
+// 音の出力状態を更新する
 // この関数を定期的に呼び出してください
 void MusicPlayer::update_sound(void) {
-  static unsigned long end_time;          // 1音符／休符をいつまで鳴らすか保持する
   unsigned long current_time = millis();  // 現在時刻
+  bool is_sounding = this->sound_end_time >= current_time;  // 音符／休符を出力中かどうか
+  
+  if (this->is_playing && this->bpm && not is_sounding) {
+    while (true) {
+      word note = this->read_note();
+      
+      float note_duration;
+      unsigned int frequency;
+      bool is_slur, is_rest;
 
-  static bool prev_is_playing = false;
-  if (this->is_playing && !prev_is_playing) { // 今回のフレームからis_playingがtrueになった場合の初期設定
-    end_time = current_time;
-  }
-
-  if (this->is_playing && this->bpm > 0 && end_time <= current_time) {  // 再生中かつ、メロディが設定済みかつ、1音符／休符が鳴らし終えている場合
-    // 新しい音符／休符を読みこみ、音を鳴らす
-    unsigned int note_duration; // 音符／休符の期間
-    unsigned int frequency;     // 周波数
-    bool is_slur, is_rest;
-
-    if (this->read_note(note_duration, frequency, is_slur, is_rest)) {  // 読み込んだ音符／休符が終了コードではないなら、その音を鳴らす
-      if (is_rest) {                    // 休符の場合
-        noTone(BUZZER_PIN);             // 音を止める
+      if (parse_note(note, this->bpm, note_duration, frequency, is_slur, is_rest)) {
+        this->sound_note(note_duration, frequency, is_slur, is_rest);
+        break;
+      } else if (this->is_looped) {
+        this->restart();
       } else {
-        if (is_slur) {                  // スラーの音符の場合
-          tone(BUZZER_PIN, frequency);  // 次の音とつなげる
-        } else {                        //　通常の音符の場合
-          unsigned int sound_duration = note_duration * SOUND_RATE + end_time - current_time; // 音を鳴らす期間
-          tone(BUZZER_PIN, frequency, sound_duration);  // 音を鳴らす
-        }
-      }
-      end_time += note_duration;        // 音符／休符を鳴らし終える時刻を設定
-      this->note_index++;
-    } else {                            // 終了コードが読み込まれた場合
-      this->stop();                     // 停止
-      if (this->is_looped) {            // ループ再生なら
-        this->play(this->is_looped);    // 再び再生
+        this->stop();
+        break;
       }
     }
   }
-
-  prev_is_playing = this->is_playing;
 }
 
 
-// 音符／休符の読み込み
-bool MusicPlayer::read_note(unsigned int& ret_note_duration, unsigned int& ret_frequency, bool& is_slur, bool& is_rest) {
-  word note = pgm_read_word(&(this->notes[this->note_index]));
-  MusicPlayer::note *n_ptr = (MusicPlayer::note *)&note;
+// 音符データの読み込み
+word MusicPlayer::read_note(void) {
+  return pgm_read_word(&(this->notes[this->note_index++]));
+}
+
+
+// 音符／休符を出力する
+void MusicPlayer::sound_note(float note_duration, unsigned int frequency, bool is_slur, bool is_rest) {
+  unsigned long current_time = millis();  // 現在時刻
+
+  static bool prev_frame_is_playing = false;
+  if (this->is_playing && not prev_frame_is_playing) { // 今回のフレームからis_playingがtrueになった場合の初期設定
+    this->sound_end_time = current_time;
+  }
+  prev_frame_is_playing = this->is_playing;
+
+  if (is_rest) {                    // 休符の場合
+    noTone(BUZZER_PIN);             // 音を止める
+  } else {
+    if (is_slur) {                  // スラーの音符の場合
+      tone(BUZZER_PIN, frequency);  // 次の音とつなげる
+    } else {                        //　通常の音符の場合
+      unsigned int sound_duration = note_duration * SOUND_RATE + this->sound_end_time - current_time; // 音を鳴らす期間
+      tone(BUZZER_PIN, frequency, sound_duration);  // 音を鳴らす
+    }
+  }
+  this->sound_end_time += (unsigned long)note_duration; // 音符／休符を鳴らし終える時刻を設定
+}
+
+
+// 音符／休符の解析
+bool parse_note(word note, unsigned int bpm, float& ret_note_duration, unsigned int& ret_frequency, bool& is_slur, bool& is_rest) {
+  note_data *n_ptr = (note_data *)&note;
 
   if (PITCH_COUNT < n_ptr->pitch) { // 異常な音階は終了コードを表す
     return false;
@@ -128,7 +163,7 @@ bool MusicPlayer::read_note(unsigned int& ret_note_duration, unsigned int& ret_f
   is_slur = (slur > 0);
 
   // 音価と付点から音を鳴らす時間を決定
-  float note_duration = 1000.0f * 60.0f / this->bpm;
+  float note_duration = 1000.0f * 60.0f / bpm;;
   switch (value) {
   case 0: note_duration *= 8;       break;  // 倍音符
   case 1: note_duration *= 4;       break;  // 全音符
@@ -140,12 +175,12 @@ bool MusicPlayer::read_note(unsigned int& ret_note_duration, unsigned int& ret_f
   case 7: note_duration /= 16;      break;  // 64分音符
   }
   switch (dot) {
-    case 0: /* note_duratioin *= 1; */  break;  // ×1
-    case 1: note_duration *= 1.5f;      break;  // ×(1 + 1/2)
-    case 2: note_duration *= 1.75f;     break;  // ×(1 + 1/2 + 1/4)
-    case 3: note_duration *= 1.875f;    break;  // ×(1 + 1/2 + 1/4 + 1/8)
+  case 0: /* note_duratioin *= 1; */  break;  // × 1
+  case 1: note_duration *= 1.5f;      break;  // ×(1 + 1/2)
+  case 2: note_duration *= 1.75f;     break;  // ×(1 + 1/2 + 1/4)
+  case 3: note_duration *= 1.875f;    break;  // ×(1 + 1/2 + 1/4 + 1/8)
   }
-  ret_note_duration = (unsigned int)note_duration;
+  ret_note_duration = note_duration;
 
   return true;
 }
--- music_player.h
+++ music_player.h
@@ -6,21 +6,11 @@
 
 class MusicPlayer {
 private:
-  // 1音
-  typedef struct {
-    unsigned value    : 3;  // 音価　　　　　0:倍音符、1:全音符、2:2分音符、3:4分音符、...、7:64分音符
-    unsigned padding1 : 1;
-    unsigned dot      : 2;  // 付点　　　　　0:付点なし(1倍)、1:付点(1.5倍)、2:2付点(1.75倍)、3:3付点(1.875倍)
-    unsigned slur     : 1;  // スラー　　　　0:なし、1:あり
-    unsigned padding2 : 1;
-    unsigned pitch    : 4;  // 音階　　　　　0:休符、1:ド、2:ド#、3:レ、...、12:シ、13~:終了コード
-    signed octave     : 4;  // オクターブ　　-8~-1:低オクターブ、0:オクターブ変動なし、1~7:高オクターブ
-  } note;
-
-private:
   int bpm = -1;
   const word *notes;        // メロディ
   bool is_playing = false;  // 再生中かどうか
+  bool is_sounding = false; // 音符／休符を出力中か
+  unsigned long sound_end_time; // 1音符／休符をいつまで鳴らすか[ms]
   bool is_looped = false;   // ループ再生かどうか
   int note_index = 0;       // 現在、メロディ中のどの音を鳴らしているか
 
@@ -29,17 +19,18 @@ public:
 
 public:
   void set_notes(int bpm, const word notes[]);  // BPMとメロディの設定
-  void play(bool is_looped);
-  void stop(void);
-  void pause(void);
+  void play(bool is_looped);  // 再生
+  void restart(void);         // 初めから再生
+  void stop(void);            // 停止
+  void pause(void);           // 一時停止
 
-  // 音を出力する
+  // 音の出力状態を更新する
   // この関数を定期的に呼び出してください
   void update_sound(void);  
 
 private:
-  bool read_note(unsigned int& ret_note_duration, unsigned int& ret_frequency, bool& is_slur, bool& is_rest); // 音符／休符の読み込み
-
+  word read_note(void); // 音符データの読み込み
+  void sound_note(float note_duration, unsigned int frequency, bool is_slur, bool is_rest); // 音符／休符を出力する
 };
 
 
